987cbe840e94117c32eb76f50590a221a8a76f8a
Leonti Bielski <prishelec@gmail.com>
2015-04-23 21:48:12 +0200

    Use testobject pitranha api from maven

--- src/test/java/org/testobject/piranha/example/Sample.java
+++ src/test/java/org/testobject/piranha/example/Sample.java
@@ -1,13 +1,20 @@
-package test;
+package org.testobject.piranha.example;
 
 import java.io.File;
 import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.jsonrpc.JSONRPCRequest;
 import org.testobject.piranha.DesiredCapabilities;
+import org.testobject.piranha.TestObjectDevice;
 import org.testobject.piranha.TestObjectPiranha;
 
 import com.citrixonline.piranha.ControlType;
@@ -18,105 +25,145 @@ import com.thoughtworks.selenium.Wait;
 
 public class Sample {
 
-    static boolean UPLOAD_APP = false;
+	static boolean UPLOAD_APP = false;
 
-    public static void main(String[] args) throws JSONException {
-        if (UPLOAD_APP == true) {
-            int frameworkAppId = TestObjectPiranha.uploadFrameworkApp("FE51FF78F8AE4729B07D3DDD8F151FBE",
-                    new File("/home/leonti/development/citrix/ForTestObject/piranha-android-server-5.0.30-SNAPSHOT.apk"));
-            System.out.println("FW app id: " + frameworkAppId);
-            return;
-        }
+	public static void main(String[] args) throws JSONException, InterruptedException, ExecutionException {
+		if (UPLOAD_APP == true) {
+			int frameworkAppId = TestObjectPiranha.uploadFrameworkApp("FE51FF78F8AE4729B07D3DDD8F151FBE", new File(
+					"/home/leonti/Downloads/AndroidPiranhaServer_1.0.apk"));
+			System.out.println("FW app id: " + frameworkAppId);
+			return;
+		}
 
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("testobject_api_key", "FE51FF78F8AE4729B07D3DDD8F151FBE");
-        capabilities.setCapability("testobject_app_id", "1");
-        capabilities.setCapability("testobject_framework_app_id", "2");
-        capabilities.setCapability("testobject_device", "Cat_B15_real");
+		List<String> devices = getAvailableDevices();
 
-        Map<String, String> piranhaCaps = new HashMap<String, String>();
-        piranhaCaps.put("className", "com.citrixonline.universal.ui.activities.LauncherActivity");
-        piranhaCaps.put("intent",
-                "com.citrixonline.piranha.androidserver/com.citrixonline.piranha.androidserver.PiranhaAndroidInstrumentation");
-        piranhaCaps.put("packageName", "com.citrixonline.android.gotomeeting");
+		Future<Void> first = performTestAsync(devices.get(0));
+		Future<Void> second = performTestAsync(devices.get(1));
 
-        capabilities.setCapability("piranha_params", new GsonBuilder().create().toJson(piranhaCaps));
+		first.get();
+		second.get();
+	}
 
-        TestObjectPiranha testObjectPiranha = null;
+	private static Future<Void> performTestAsync(final String deviceId) {
+		return Executors.newFixedThreadPool(1).submit(new Callable<Void>() {
 
-        try {
+			@Override
+			public Void call() {
 
-            testObjectPiranha = new TestObjectPiranha(capabilities);
+				performTest(deviceId);
+				return null;
+			}
+		});
+	}
 
-            int port = testObjectPiranha.getPort();
-            //final PiranhaAndroidClient c = new PiranhaAndroidClient(ip, 7100, true);
-            System.out.println("Port is: " + port);
-            final PiranhaAndroidClient c = new PiranhaAndroidClient("localhost", port, true);
+	private static void performTest(String deviceId) {
 
-            sleep(10000);
+		System.out.println("Performing test on " + deviceId);
 
-            boolean ret1 = c.robotium().waiter().waitForViewToBeEnabled(ControlType.TEXT, "</#JoinMeetingId/>", 30);
+		DesiredCapabilities capabilities = new DesiredCapabilities();
+		capabilities.setCapability("testobject_api_key", "<YOUR KEY>");
+		capabilities.setCapability("testobject_app_id", "1");
+		capabilities.setCapability("testobject_framework_app_id", "2");
+		capabilities.setCapability("testobject_device", deviceId);
 
-            takeScreenshot(c.robotium());
+		Map<String, String> piranhaCaps = new HashMap<String, String>();
+		piranhaCaps.put("className", "com.citrixonline.universal.ui.activities.LauncherActivity");
+		piranhaCaps
+				.put("intent",
+						"com.citrixonline.piranha.androidserver/com.citrixonline.piranha.androidserver.PiranhaAndroidInstrumentation");
+		piranhaCaps.put("packageName", "com.citrixonline.android.gotomeeting");
 
-            if (ret1) {
-                c.robotium().setter().clearEditText("</#JoinMeetingId/>");
+		capabilities.setCapability("piranha_params", new GsonBuilder().create().toJson(piranhaCaps));
 
-                c.robotium().setter().setText("</#JoinMeetingId/>", "555-000-000");
-            }
+		TestObjectPiranha testObjectPiranha = null;
 
-            takeScreenshot(c.robotium());
+		try {
 
-            new Wait() {
+			testObjectPiranha = new TestObjectPiranha(capabilities);
 
-                @Override
-                public boolean until() {
-                    c.robotium().clicker().clickImageView("</#JoinMeetingButton/>");
-                    try {
-                        Thread.sleep(10000);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
+			int port = testObjectPiranha.getPort();
+			// final PiranhaAndroidClient c = new PiranhaAndroidClient(ip, 7100,
+			// true);
+			System.out.println("Port is: " + port + ", session id is '" + testObjectPiranha.getSessionId() + "' ("
+					+ deviceId + ")");
+			final PiranhaAndroidClient c = new PiranhaAndroidClient("localhost", port, false);
 
-                    takeScreenshot(c.robotium());
+			sleep(15000);
 
-                    return c.robotium().waiter()
-                            .waitForViewToBeEnabled(ControlType.BUTTON, "OK", 5);
-                }
-            }.wait("Error Dialog did not show up", 60 * 1000, 2 * 1000);
+			boolean ret1 = c.robotium().waiter().waitForViewToBeEnabled(ControlType.TEXT, "</#JoinMeetingId/>", 30);
 
-            c.robotium().clicker().clickButton("OK");
+			takeScreenshot(c.robotium());
 
-            sleep(5000);
+			if (ret1) {
+				c.robotium().setter().clearEditText("</#JoinMeetingId/>");
 
-            takeScreenshot(c.robotium());
+				c.robotium().setter().setText("</#JoinMeetingId/>", "555-000-000");
+			}
 
-        } finally {
-            if (testObjectPiranha != null) {
-                testObjectPiranha.close();
-            }
-        }
+			takeScreenshot(c.robotium());
 
-    }
+			new Wait() {
 
-    private static void sleep(long time) {
-        try {
-            Thread.sleep(time);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
+				@Override
+				public boolean until() {
+					c.robotium().clicker().clickImageView("</#JoinMeetingButton/>");
+					try {
+						Thread.sleep(10000);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
 
-    private static void takeScreenshot(RobotiumClient roboriumClient) {
-        try {
-            JSONArray params = new JSONArray();
-            JSONRPCRequest request = new JSONRPCRequest("PiranhaSystemUtils::takeScreenShot", params);
-            roboriumClient.execute(request);
-        } catch (JSONException e) {
-            throw new RuntimeException(e);
-        }
-    }
+					takeScreenshot(c.robotium());
+
+					return c.robotium().waiter().waitForViewToBeEnabled(ControlType.BUTTON, "OK", 5);
+				}
+			}.wait("Error Dialog did not show up", 60 * 1000, 2 * 1000);
+
+			c.robotium().clicker().clickButton("OK");
+
+			sleep(5000);
+
+			takeScreenshot(c.robotium());
+
+			System.out.println("Test if succesfully finished on " + deviceId);
+
+		} catch (Exception e) {
+			System.out.println("Test failed on " + deviceId + " because of " + e.getMessage());
+		} finally {
+			if (testObjectPiranha != null) {
+				testObjectPiranha.close();
+			}
+		}
+	}
+
+	private static List<String> getAvailableDevices() {
+		List<String> available = new LinkedList<>();
+
+		for (TestObjectDevice device : TestObjectPiranha.listDevices()) {
+			if (device.isAvailable && device.apiLevel >= 17) {
+				available.add(device.id);
+			}
+		}
+
+		return available;
+	}
+
+	private static void sleep(long time) {
+		try {
+			Thread.sleep(time);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private static void takeScreenshot(RobotiumClient roboriumClient) {
+		try {
+			JSONArray params = new JSONArray();
+			JSONRPCRequest request = new JSONRPCRequest("PiranhaSystemUtils::takeScreenShot", params);
+			roboriumClient.execute(request);
+		} catch (JSONException e) {
+			throw new RuntimeException(e);
+		}
+	}
 
 }
-
-
